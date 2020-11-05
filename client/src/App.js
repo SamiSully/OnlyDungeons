6296541468674fb5df6979b3ad19653c7a264db3
Lexx <joncanales2@gmail.com>
2020-11-05 10:36:37 -0500

    Initial navbar basis done moving to form components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <h1> This is a page!</h1>
       </header>
     </div>
